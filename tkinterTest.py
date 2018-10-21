7b90834e8c3d68242a7569847ae3af4386c93d16
mushroomchen0429 <32380829+mushroomchen0429@users.noreply.github.com>
2018-10-22 01:19:47 +0800

    Add files via upload

---
+++ tkinterTest.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+window = tk.Tk()
+window.title('my window')
+window.geometry('300x100')
+window.mainloop()
+l = tk.Label(window,
+             text='OMG! this is TK!',    # 标签的文字
+             bg='green',     # 背景颜色
+             font=('Arial', 12),     # 字体和字体大小
+             width=15, height=2  # 标签长宽
+             )
+l.pack()    # 固定窗口位置
